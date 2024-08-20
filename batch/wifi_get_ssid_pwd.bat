e6e497d2e8e7d06d5248728b8066c685e275b998
Earlam <63388059+JimmyGaubert@users.noreply.github.com>
2024-08-21 01:24:49 +0200

    Add files via upload

---
+++ batch/wifi_get_ssid_pwd.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal EnableDelayedExpansion
+for /f "tokens=2 delims=: " %%A in ('netsh wlan show interfaces ^| findstr "SSID"') do ( set "ssid=%%A" )
+if "%ssid%"=="" (
+    echo No network interface found!
+    pause
+    exit /b 0
+) else (
+    for /f "tokens=2 delims=:" %%A in ('netsh wlan show profiles ^| findstr /i /c:"%ssid%"') do (
+        set "profile=%%A"
+        set "profile=!profile:~1!"
+        if "!profile!"=="" (
+            rem lulz
+        ) else (
+            cls
+            call netsh wlan show profiles name="!profile!" key=clear | findstr /i "interface Wi-Fi"
+            call netsh wlan show profiles name="!profile!" key=clear | findstr /i "Contenu" | for /f "tokens=2 delims=:" %%B in ('findstr /r /c:"Contenu"') do @echo %%B
+        )
+    )
+    pause >nul
+    exit /b 0
+)
